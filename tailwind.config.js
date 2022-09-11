5854ab9c561dedd5b830e344cd2a07a1a97d917c
alek101 <47416711+alek101@users.noreply.github.com>
2022-09-11 08:12:07 +0200

    prepisano sve

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "weather-primary":"#00668A",
-        "weather-secondary":"#004E71"
-      }
+        "weather-primary": "#00668A",
+        "weather-secondary": "#004E71",
+      },
     },
     fontFamily: {
-      Roboto : ["Roboto, sans-serif"],
+      Roboto: ["Roboto, sans-serif"],
     },
     container: {
       padding: "2rem",
@@ -18,7 +17,7 @@ module.exports = {
     screens: {
       sm: "640px",
       md: "768px",
-    }
+    },
   },
   plugins: [],
 };
