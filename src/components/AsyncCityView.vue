18b52febf903003c2d796547c1a44ddee1928bb3
alek101 <47416711+alek101@users.noreply.github.com>
2022-09-11 08:09:52 +0200

    mid lession 8

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+    import axios from 'axios';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const getWeatherData = async () => {
+    try {
+        const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`)
+    } catch(err) {
+        console.log(err)
+    }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,8 +12,10 @@
         <p v-if="searchError">Sorry something went wrong</p>
         <p v-if="!searchError && mapboxsearchResults.lenght == 0">No match results</p>
         <template v-else>
-            <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id"
-          class="py-2 cursor-pointer">
+            <li v-for="searchResult in mapboxSearchResults" 
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)">
           
             {{ searchResult.place_name }}
           </li>
@@ -29,6 +31,21 @@
 <script setup>
     import { ref } from "vue";
     import axios from "axios";
+    import {userRouter} from 'vue-router';
+import router from "../router";
+
+    const previewCity = (searchResult) => {
+      const [city,state] = searchResult.place_name.split(",");
+      router.push({
+        name: 'cityView',
+        params: {state: state.replaceAll(" ",""), city: city},
+        query : {
+          lat: searchResult.geometry.coordinates[1],
+          lng: searchResult.geometry.coordinates[0],
+          preview: true
+        }
+      })
+    };
 
     const searchQuery = ref("");
     const queryTimeout = ref(null);
